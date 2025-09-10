717d448dc004141b6a68937b7c3b437d8f04911c
sstbiaanca <144484262+sstbiaanca@users.noreply.github.com>
2025-09-10 20:37:03 -0300

    Update PedidoService.java

--- src/services/PedidoService.java
+++ src/services/PedidoService.java
@@ -1,4 +1,11 @@
 package services;
 
+import models.Carrinho;
+import models.Pedido;
+import models.MeioPagamento;
+
 public interface PedidoService {
+    Pedido criarPedido(Carrinho carrinho, MeioPagamento pagamento);
+    Pedido buscarPorId(int id);
+    void atualizarStatus(int idPedido, String novoStatus);
 }
