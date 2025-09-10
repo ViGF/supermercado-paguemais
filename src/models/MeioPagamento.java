c0d27c48c1826e6b007dde01185c86d042784f85
sstbiaanca <144484262+sstbiaanca@users.noreply.github.com>
2025-09-10 20:13:43 -0300

    Update MeioPagamento.java

--- src/models/MeioPagamento.java
+++ src/models/MeioPagamento.java
@@ -2,5 +2,10 @@ package models;
 
 public class MeioPagamento {
     private int idMeioPagamento;
-    private int idCliente; //poderia ser Tipo, falta terminar
+    private int idCliente;//poderia ser Tipo, falta terminar
+    
+    public class MeioPagamento(int idMeioPagamento, Cliente cliente){
+    this.idMeioPagamento = idMeioPagamento;
+    this.cliente = cliente;
+    }
 }
