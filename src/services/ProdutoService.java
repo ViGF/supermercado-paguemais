9dd031c664a1f152e4e77c23c7be3b36bd808865
sstbiaanca <144484262+sstbiaanca@users.noreply.github.com>
2025-09-10 20:35:28 -0300

    Update ProdutoService.java

--- src/services/ProdutoService.java
+++ src/services/ProdutoService.java
@@ -1,4 +1,12 @@
 package services;
 
+import models.Produto;
+import java.util.List;
+
 public interface ProdutoService {
+    void cadastrar(Produto produto);
+    Produto buscarPorId(int id);
+    List<Produto> listarTodos();
+    void atualizar(Produto produto);
+    void deletar(int id);
 }
