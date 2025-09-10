72e5b22ca68bdf3020b223d5f312ad0bb62f5e4a
sstbiaanca <144484262+sstbiaanca@users.noreply.github.com>
2025-09-10 20:37:32 -0300

    Update CartaoService.java

--- src/services/CartaoService.java
+++ src/services/CartaoService.java
@@ -1,4 +1,9 @@
 package services;
 
+import models.Cartao;
+
 public interface CartaoService {
+    void cadastrarCartao(Cartao cartao);
+    boolean validarCartao(Cartao cartao);
+    void removerCartao(int idCartao);
 }
