e5c46012b8007c2e4f7c21717edcefdcb138889e
sstbiaanca <144484262+sstbiaanca@users.noreply.github.com>
2025-09-10 20:36:04 -0300

    Update EstoqueService.java

--- src/services/EstoqueService.java
+++ src/services/EstoqueService.java
@@ -1,4 +1,9 @@
 package services;
 
+import models.Produto;
+
 public interface EstoqueService {
+    void adicionarProduto(Produto produto, int quantidade);
+    void removerProduto(int idProduto, int quantidade);
+    int consultarEstoque(int idProduto);
 }
