36aeab8d509488b8c1de77073934095eeeafbddf
sstbiaanca <144484262+sstbiaanca@users.noreply.github.com>
2025-09-10 20:36:38 -0300

    Update CarrinhoService.java

--- src/services/CarrinhoService.java
+++ src/services/CarrinhoService.java
@@ -1,4 +1,10 @@
 package services;
 
+import models.Carrinho;
+import models.Produto;
+
 public interface CarrinhoService {
+    void adicionarProduto(Carrinho carrinho, Produto produto, int quantidade);
+    void removerProduto(Carrinho carrinho, Produto produto);
+    double calcularTotal(Carrinho carrinho);
 }
