a0612cd91cfdd3b7ab46903c9265a4df6057f7ff
Amanda Silva <amandadasilvapereira162004@gmail.com>
2026-03-15 18:02:36 -0300

    produto teste

---
+++ src/test/java/com/example/supermercado_paguemais/repository/ProdutoRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.example.supermercado_paguemais.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProdutoRepositoryTest {
+
+    @Test
+    void findByNomeProdutoContainingIgnoreCase() {
+    }
+
+    @Test
+    void findByCategoriaIgnoreCase() {
+    }
+}
