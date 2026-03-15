180647a6d407a8e4f497b47158503582609afa10
Amanda Silva <amandadasilvapereira162004@gmail.com>
2026-03-15 19:04:52 -0300

    teste produto

--- src/main/java/com/example/supermercado_paguemais/repository/ProdutoRepository.java
+++ src/main/java/com/example/supermercado_paguemais/repository/ProdutoRepository.java
@@ -2,6 +2,7 @@ package com.example.supermercado_paguemais.repository;
 
 import com.example.supermercado_paguemais.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
