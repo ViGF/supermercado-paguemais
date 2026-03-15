f083d9ce2bc359769fbc04ff7a2f3cd4d671434e
Amanda Silva <amandadasilvapereira162004@gmail.com>
2026-03-15 17:44:25 -0300

    application-test

---
+++ src/test/java/com/example/supermercado_paguemais/repository/ProdutoRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.example.supermercado_paguemais.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class ProdutoRepositoryTest {
+
+    @Test
+    void findByNomeProdutoContainingIgnoreCase() {
+
+    }
+
+    @Test
+    void findByCategoriaIgnoreCase() {
+
+    }
+}
